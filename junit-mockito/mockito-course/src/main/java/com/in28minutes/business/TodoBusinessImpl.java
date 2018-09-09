4aad238f656df800921bfada062e5e2ac6a080ab
bopa <bogdan.padure@mimacom.com>
2018-09-09 20:28:44 +0200

    Setting up SUT

---
+++ junit-mockito/mockito-course/src/main/java/com/in28minutes/business/TodoBusinessImpl.java
@@ -0,0 +1,26 @@
+package com.in28minutes.business;
+
+import com.in28munites.data.api.TodoService;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TodoBusinessImpl {
+    private TodoService todoService;
+
+    public TodoBusinessImpl(TodoService todoService) {
+        this.todoService = todoService;
+    }
+
+    public List<String> retriveTodosRelatedToSpring(String  user){
+        List<String> filteredTodos = new ArrayList<String>();
+        List<String> todos = todoService.retriveTodos(user);
+
+        for (String todo: todos) {
+            if(todo.contains("Spring")){
+                filteredTodos.add(todo);
+            }
+        }
+        return filteredTodos;
+    }
+}
